25b47455a3a526d8536d236bc828dc11018624ce
Victor Erukhimov <victor.erukhimov@itseez3d.com>
2019-09-15 18:34:05 +0300

    Fixed an issue with the debug configuration

--- vxa-config.cmake
+++ vxa-config.cmake
@@ -1,4 +1,4 @@
 # vxa-config.cmake - package configuration file
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/vxa.cmake)
+include(${SELF_DIR}/vxa.cmake)
